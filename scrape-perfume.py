3b34cbaf6e10603ef23d6c035712e61c673edf18
dzchen314 <37160919+dzchen314@users.noreply.github.com>
2018-04-09 01:34:01 -0400

    Update scrape-perfume.py

--- scrape-perfume.py
+++ scrape-perfume.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+from multiprocessing.dummy import Pool
 import os
 import time
 import requests
@@ -18,16 +19,32 @@ FILENAME = 'perfume-data'
 class Scraper():
     """Scraper for basenotes.com"""
 
-    def __init__(self, pages_scraped=(1,1)):
+    def __init__(self, pages_scraped=(1,1), num_jobs=1, clear_old_data=True):
         self.pages_scraped = pages_scraped
+        self.num_jobs = num_jobs
+        self.clear_old_data = clear_old_data
         self.session = requests.Session()
-        self.est_reviews = (pages_scraped[1] + 1 - pages_scraped[0]) * 30 #there are 30 reviews per page
+        self.est_reviews = (pages_scraped[1] + 1 - pages_scraped[0]) * 30
         self.review_count = 0
         self.start_time = time.time()
+        
+        if num_jobs > 1:
+            self.multiprocessing = True
+            self.worker_pool = Pool(num_jobs)
+        else:
+            self.multiprocessing = False
 
     def scrape_site(self):
-        for page in range(self.pages_scraped[0], self.pages_scraped[1] + 1):
-            self.scrape_page(BASE_URL.format(page))
+        if self.clear_old_data:
+            self.clear_data_dir()
+        if self.multiprocessing:
+            link_list = [BASE_URL.format(page) for page in range(self.pages_scraped[0],self.pages_scraped[1] + 1)]
+            records = self.worker_pool.map(self.scrape_page, link_list)
+            self.worker_pool.terminate()
+            self.worker_pool.join()
+        else:
+            for page in range(self.pages_scraped[0], self.pages_scraped[1] + 1):
+                self.scrape_page(BASE_URL.format(page))
         print('Scrape finished...')
         self.condense_data()
 
@@ -59,16 +76,52 @@ class Scraper():
             split_review = str(review).split('</h2>')
             review_text = split_review[len(split_review)-1][0:-6]
             
-            #right now I am pulling name, maker, and review text. I plan to add rating and scents
+            #parse information
+            rating, year, gender, availability = self.scrape_info(review_url)
+            
             review_data = {
             'perfume': perfume_name,
             'maker': perfume_maker,
-            'review': review_text
+            'review': review_text,
+            'rating': rating,
+            'year': year,
+            'gender': gender,
+            'availability': availability
             }
              
             scrape_data.append(review_data)
             self.update_scrape_status()
         self.save_data(scrape_data)
+        
+    def scrape_info(self, review_url):
+        info_response = self.session.get(review_url, headers=HEADERS)
+        info_soup = BeautifulSoup(info_response.content, 'html.parser')
+        info = info_soup.find_all('div', {'class': 'peoplelist'})[0:]
+        try:
+            rating = info[0].find('meta')['content']
+        except TypeError:
+            rating = float('nan')
+        info_parse = str(info[0:]).split('<td>')
+        info_parse = remove_brackets(info_parse)
+        year = info_parse[1][14:]
+        gender = info_parse[2][6:]
+        availability = info_parse[3][12:]
+        return rating, year, gender, availability
+            
+    def remove_brackets(input_list):
+        ret = [None]*len(input_list)
+        skip1c = 0
+        for j in range(0,len(input_list)-1):
+            words = ''
+            for i in input_list[j]:
+                if i == '<':
+                    skip1c += 1
+                elif i == '>' and skip1c > 0:
+                        skip1c -= 1
+                elif skip1c == 0:
+                    words += i
+            ret[j] = words
+        return ret
 
     def save_data(self, data):
         filename = '{}/{}_{}.json'.format(DATA_DIR, FILENAME, time.time())
@@ -78,6 +131,22 @@ class Scraper():
             pass
         with open(filename, 'w') as fout:
             json.dump(data, fout)
+    
+    def clear_all_data(self):
+        self.clear_data_dir()
+        self.clear_output_data()
+
+    def clear_data_dir(self):
+        try:
+            shutil.rmtree(DATA_DIR)
+        except FileNotFoundError:
+            pass
+
+    def clear_output_data(self):
+        try:
+            os.remove('{}.json'.format(FILENAME))
+        except FileNotFoundError:
+            pass
 
     def condense_data(self):
         print('Condensing Data...')
@@ -99,6 +168,6 @@ class Scraper():
 
 if __name__ == '__main__':
     pages = (1, 1)
-    perfume_scraper = Scraper(pages_scraped = pages)
+    perfume_scraper = Scraper(pages_scraped = pages, num_jobs=4)
 
     perfume_scraper.scrape_site()
